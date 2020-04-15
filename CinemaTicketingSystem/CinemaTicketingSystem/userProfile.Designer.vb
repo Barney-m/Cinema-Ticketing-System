d611ef83c1b5569a9c52be5518621d189daff04c
Gaabu <junjet99@gmail.com>
2020-04-15 15:44:28 +0800

    profile

--- CinemaTicketingSystem/CinemaTicketingSystem/userProfile.Designer.vb
+++ CinemaTicketingSystem/CinemaTicketingSystem/userProfile.Designer.vb
@@ -23,16 +23,16 @@ Partial Class userProfile
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.txtName = New System.Windows.Forms.TextBox()
-        Me.txtPhone = New System.Windows.Forms.TextBox()
-        Me.txtDob = New System.Windows.Forms.TextBox()
-        Me.txtEmail = New System.Windows.Forms.TextBox()
         Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtDob = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.btnChgPass = New System.Windows.Forms.Button()
         Me.btnSave = New System.Windows.Forms.Button()
         Me.btnEdit = New System.Windows.Forms.Button()
@@ -42,6 +42,7 @@ Partial Class userProfile
         '
         'Panel1
         '
+        Me.Panel1.BackColor = System.Drawing.Color.DarkGray
         Me.Panel1.Controls.Add(Me.txtAddress)
         Me.Panel1.Controls.Add(Me.txtEmail)
         Me.Panel1.Controls.Add(Me.txtDob)
@@ -57,93 +58,109 @@ Partial Class userProfile
         Me.Panel1.Size = New System.Drawing.Size(476, 325)
         Me.Panel1.TabIndex = 0
         '
-        'Label1
+        'txtAddress
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(89, 39)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(53, 17)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Name :"
+        Me.txtAddress.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtAddress.Enabled = False
+        Me.txtAddress.Location = New System.Drawing.Point(165, 236)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(214, 50)
+        Me.txtAddress.TabIndex = 8
         '
-        'Label2
+        'txtEmail
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(63, 87)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(79, 17)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Birth Date :"
+        Me.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtEmail.Enabled = False
+        Me.txtEmail.Location = New System.Drawing.Point(165, 186)
+        Me.txtEmail.Multiline = True
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(214, 25)
+        Me.txtEmail.TabIndex = 7
         '
-        'Label3
+        'txtDob
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(63, 136)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(79, 17)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Phone No :"
+        Me.txtDob.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtDob.Enabled = False
+        Me.txtDob.Location = New System.Drawing.Point(165, 84)
+        Me.txtDob.Multiline = True
+        Me.txtDob.Name = "txtDob"
+        Me.txtDob.Size = New System.Drawing.Size(214, 25)
+        Me.txtDob.TabIndex = 6
         '
-        'Label4
+        'txtPhone
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(92, 189)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(50, 17)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Email :"
+        Me.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtPhone.Enabled = False
+        Me.txtPhone.Location = New System.Drawing.Point(165, 133)
+        Me.txtPhone.Multiline = True
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(214, 25)
+        Me.txtPhone.TabIndex = 5
+        '
+        'txtName
+        '
+        Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtName.Enabled = False
+        Me.txtName.Location = New System.Drawing.Point(165, 36)
+        Me.txtName.Multiline = True
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(214, 25)
+        Me.txtName.TabIndex = 1
         '
         'Label5
         '
         Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Bahnschrift SemiBold", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.Location = New System.Drawing.Point(74, 239)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(68, 17)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "Address :"
         '
-        'txtName
+        'Label4
         '
-        Me.txtName.Location = New System.Drawing.Point(176, 36)
-        Me.txtName.Multiline = True
-        Me.txtName.Name = "txtName"
-        Me.txtName.Size = New System.Drawing.Size(203, 25)
-        Me.txtName.TabIndex = 1
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Bahnschrift SemiBold", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(92, 189)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Email :"
         '
-        'txtPhone
+        'Label3
         '
-        Me.txtPhone.Location = New System.Drawing.Point(176, 133)
-        Me.txtPhone.Multiline = True
-        Me.txtPhone.Name = "txtPhone"
-        Me.txtPhone.Size = New System.Drawing.Size(203, 25)
-        Me.txtPhone.TabIndex = 5
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Bahnschrift SemiBold", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(63, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Phone No :"
         '
-        'txtDob
+        'Label2
         '
-        Me.txtDob.Location = New System.Drawing.Point(176, 84)
-        Me.txtDob.Multiline = True
-        Me.txtDob.Name = "txtDob"
-        Me.txtDob.Size = New System.Drawing.Size(203, 25)
-        Me.txtDob.TabIndex = 6
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Bahnschrift SemiBold", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(63, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Birth Date :"
         '
-        'txtEmail
+        'Label1
         '
-        Me.txtEmail.Location = New System.Drawing.Point(176, 186)
-        Me.txtEmail.Multiline = True
-        Me.txtEmail.Name = "txtEmail"
-        Me.txtEmail.Size = New System.Drawing.Size(203, 25)
-        Me.txtEmail.TabIndex = 7
-        '
-        'txtAddress
-        '
-        Me.txtAddress.Location = New System.Drawing.Point(176, 236)
-        Me.txtAddress.Multiline = True
-        Me.txtAddress.Name = "txtAddress"
-        Me.txtAddress.Size = New System.Drawing.Size(203, 50)
-        Me.txtAddress.TabIndex = 8
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Bahnschrift SemiBold", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(89, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name :"
         '
         'btnChgPass
         '
+        Me.btnChgPass.Font = New System.Drawing.Font("Times New Roman", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnChgPass.Location = New System.Drawing.Point(130, 403)
         Me.btnChgPass.Name = "btnChgPass"
         Me.btnChgPass.Size = New System.Drawing.Size(181, 34)
@@ -153,16 +170,19 @@ Partial Class userProfile
         '
         'btnSave
         '
-        Me.btnSave.Location = New System.Drawing.Point(612, 246)
+        Me.btnSave.Font = New System.Drawing.Font("Times New Roman", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(626, 246)
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(106, 34)
         Me.btnSave.TabIndex = 2
         Me.btnSave.Text = "Save"
         Me.btnSave.UseVisualStyleBackColor = True
+        Me.btnSave.Visible = False
         '
         'btnEdit
         '
-        Me.btnEdit.Location = New System.Drawing.Point(612, 296)
+        Me.btnEdit.Font = New System.Drawing.Font("Times New Roman", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.Location = New System.Drawing.Point(626, 296)
         Me.btnEdit.Name = "btnEdit"
         Me.btnEdit.Size = New System.Drawing.Size(106, 34)
         Me.btnEdit.TabIndex = 3
@@ -171,7 +191,8 @@ Partial Class userProfile
         '
         'btnCancel
         '
-        Me.btnCancel.Location = New System.Drawing.Point(612, 349)
+        Me.btnCancel.Font = New System.Drawing.Font("Times New Roman", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(626, 348)
         Me.btnCancel.Name = "btnCancel"
         Me.btnCancel.Size = New System.Drawing.Size(106, 34)
         Me.btnCancel.TabIndex = 4
--- CinemaTicketingSystem/CinemaTicketingSystem/userProfile.vb
+++ CinemaTicketingSystem/CinemaTicketingSystem/userProfile.vb
@@ -1,3 +1,11 @@
 ﻿Public Class userProfile
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        btnSave.Visible = True
+        txtAddress.Enabled = True
+        txtEmail.Enabled = True
+        txtPhone.Enabled = True
+
+    End Sub
+
 
 End Class
