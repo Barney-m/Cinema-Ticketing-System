48ddc2fd1a418445f7f1261cbb5a34ce33cdfe66
Gaabu <junjet99@gmail.com>
2020-04-19 22:22:01 +0800

    pepehand

--- CinemaTicketingSystem/CinemaTicketingSystem/My Project/Application.Designer.vb
+++ CinemaTicketingSystem/CinemaTicketingSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CinemaTicketingSystem.chgPassword
+            Me.MainForm = Global.CinemaTicketingSystem.AdminHome
         End Sub
     End Class
 End Namespace
